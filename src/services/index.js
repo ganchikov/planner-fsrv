f517b59ae8d8bc00dee7fb1d42da64e59942bb23
Ganchikov <MGanchikov@DBDC.luxoft.com>
2018-04-24 20:35:49 +0300

    basic authentication added

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@ const idgenerator = require('./idgenerator/idgenerator.service.js');
 const people = require('./people/people.service.js');
 const absences = require('./absences/absences.service.js');
 const dataLoader = require('./data-loader/data-loader.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(teams);
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.configure(people);
   app.configure(absences);
   app.configure(dataLoader);
+  app.configure(users);
 };
