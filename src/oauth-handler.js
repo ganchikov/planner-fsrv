8cc20772c44eeecf6918d320600545cbabcb1a03
Ganchikov <MGanchikov@DBDC.luxoft.com>
2018-05-04 19:21:52 +0300

    started work on poster to auth0

--- src/oauth-handler.js
+++ src/oauth-handler.js
@@ -5,9 +5,18 @@ module.exports = function (app) {
         jwt: config.jwt,
         secret: config.secret
       };
+
   
       return function (req, res, next) {
         if (req.feathers && req.feathers.payload) {
+
+          const auth0route = new String(config.auth0.domain).concat('/oauth/token');
+          const clientId = config.auth0.clientID;
+          const clientSecret = config.auth0.clientSecret;
+          const redirect_uri = '/';
+          const code = req.feathers.payload.code;
+
+
           app.passport.createJWT(req.feathers.payload, options).then(token => {
             res.redirect(`${url}?token=${token}`);
           })
