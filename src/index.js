468fe7b6eb2a84ed48497aa9c611d0ccc0c09c6c
Ganchikov <MGanchikov@DBDC.luxoft.com>
2018-05-31 18:28:18 +0300

    https support added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-console */
+const fs = require('fs');
+const https = require('https');
 const logger = require('./config/winston');
 const app = require('./app');
 const port = app.get('port');
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync('./.sslcert/localhost.key'),
+  cert: fs.readFileSync('./.sslcert/localhost.cert')
+}, app).listen(443);
+
+httpsServer.on('listening', () =>
+  logger.info('Feathers secured application started on https://%s:443', app.get('host'))
+);
+
+app.setup(httpsServer);
+
 const server = app.listen(port);
 
 /// TODO: only for dev testign purposes - don't forget to remove -> shift to default config section
