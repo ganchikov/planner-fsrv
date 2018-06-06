ff750aee69cb2ae478b35a03797703c87da0f45f
Ganchikov <MGanchikov@DBDC.luxoft.com>
2018-06-06 16:24:20 +0300

    build config fix

--- .circleci/pushConfig.sh
+++ .circleci/pushConfig.sh
@@ -2,7 +2,7 @@ export CIRCLE_TOKEN=8510380802b037682159311644f3006ef40e08ad
 curl --user ${CIRCLE_TOKEN}: \
     --insecure \
     --request POST \
-    --form revision=edcb1a11e845c9e5b6d5e137dbb45365adee6989\
+    --form revision=eb60a22d3fe6dc2f2194ca2ba8ee750db936fb9f\
     --form config=@test_config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/ganchikov/planner-fsrv/tree/master
