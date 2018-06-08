5743a49cd747e199c88cb89f3b23754d55009894
Ganchikov <MGanchikov@DBDC.luxoft.com>
2018-06-08 18:01:41 +0300

    appver test fix, build increment job added to commit workflow

--- test/services/appver.test.js
+++ test/services/appver.test.js
@@ -1,10 +1,19 @@
 const assert = require('assert');
 const app = require('../../src/app');
+const routeBuilder = require('../../src/helpers/routebuilder');
+const {appver} = require('../../src/constants/services');
 
 describe('\'appver\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('appver');
+  
+  const service = app.service(routeBuilder(app, appver));
 
+  it('registered the service', () => {
     assert.ok(service, 'Registered the service');
+  });
+
+  it('retrieves the application version & build number', async () => {
+    const result = await service.find();
+    assert.ok(result.hasOwnProperty('version'), 'version retrieved');
+    assert.ok(result.hasOwnProperty('build'), 'build retrieved');
   });
 });
