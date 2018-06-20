823e729f5a6b719df5fda00d095d1987e7c43396
ganchikov <ganchikovm@gmail.com>
2018-06-20 13:45:31 +0300

    config updated to disable https for non-prod configs, build script updated - manual step for deployment added

--- src/https.js
+++ src/https.js
@@ -2,18 +2,23 @@ const fs = require('fs');
 const https = require('https');
 
 module.exports = function (app) {
-const logger = app.get('logger');
-const sslCertPath = app.get('sslCertPath');
-
-if (!fs.existsSync(sslCertPath.concat('/localhost.key') || !fs.existsSync(sslCertPath.concat('/localhost.cert')))) {
-    logger.info('SSL cert is missing, cannot start https mode');
+  const logger = app.get('logger');
+  const sslSettings = app.get('ssl');
+  if (!sslSettings.enabled) {
+    logger.info('SSL is disabled');
     return;
-}
+  }
+  const sslCertPath = sslSettings.sslCertPath;
 
-const httpsServer = https.createServer({
-    key: fs.readFileSync('./.sslcert/domain-key.key'),
-    cert: fs.readFileSync('./.sslcert/domain-crt.cert')
-  }, app).listen(443);
+  if (!fs.existsSync(sslCertPath.concat('/localhost.key') || !fs.existsSync(sslCertPath.concat('/localhost.cert')))) {
+      logger.info('SSL cert is missing, cannot start https mode');
+      return;
+  }
+
+  const httpsServer = https.createServer({
+      key: fs.readFileSync('./.sslcert/domain-key.key'),
+      cert: fs.readFileSync('./.sslcert/domain-crt.cert')
+    }, app).listen(443);
   
   httpsServer.on('listening', () =>
     logger.info('Feathers secured application started on https://%s:443', app.get('host'))
