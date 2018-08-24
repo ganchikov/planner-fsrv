786bc1110f6c30f0c698f3ccf45e056777a3e0d8
Mikhail Ganchikov <ganchikovm@gmail.com>
2018-08-24 10:01:34 +0300

    added tests for team service

--- test/services/teams.test.js
+++ test/services/teams.test.js
@@ -1,12 +1,57 @@
 const assert = require('assert');
 const app = require('@src/app');
 const routeBuilder = require('@helpers/routebuilder');
-const {teams} = require('@constants/services');
+const {teams, people, authenticate} = require('@constants/services');
+const {authentication} = require('@constants/config');
+const jwtGen = require('@helpers/jwt-gen');
+
 
 describe('\'teams\' service', () => {
-  it('registered the service', () => {
-    const service = app.service(routeBuilder(app, teams));
+  const authService = app.service(routeBuilder(app, authenticate));
+  const teamService = app.service(routeBuilder(app, teams));
+  const peopleService = app.service(routeBuilder(app, people));
 
-    assert.ok(service, 'Registered the service');
+  let authConfig = app.get(authentication);
+  const jwt = new jwtGen(authConfig);
+  const headers = {authorization: 'BEARER ' + jwt.getAccessToken().compact()};
+  let teamRecord = null;
+  let teamId = null;
+  let personId = null;
+
+  it('registered the service', async () => {
+    await authService.create({id_token: jwt.getIdToken().compact()}, {headers});
+    const personResult = await peopleService.create({name: 'Test Person'}, {headers});
+    personId = personResult._id;
+    assert.ok(teamService, 'Service not registered');
   });
+
+  it('creates the team record without persons', async () => {
+    const teamObject = {
+      name: 'Test team',
+      people: []
+    };
+    teamRecord = await teamService.create(teamObject, {headers});
+    assert.ok(teamRecord, 'team not created');
+    teamId = teamRecord._id;
+  });
+
+  it('updates the team record with person included', async() => {
+    teamRecord.members.push(personId);
+    teamRecord = await teamService.patch(teamId, teamRecord, {headers});
+    assert.equal(teamRecord.members[0].toString(), personId.toString(), 'team record not updated with person');
+  });
+
+  it('get the team record with child person record', async () => {
+    teamRecord = await teamService.get(teamId, {headers});
+    assert.equal(teamRecord.members[0]._id.toString(), personId.toString(), 'wrong person record retrieved for team');
+    assert.equal(teamRecord.members[0].name, 'Test Person', 'child person record details not fetched');
+  });
+
+  it('removes the team record without child person record removal', async () => {
+    let result = await teamService.remove(teamId, {headers});
+    assert.equal(result._id.toString(), teamId.toString(), 'Wrong team id removed!');
+    result = await peopleService.find({query: {_id: personId}, headers});
+    assert.equal(result.total, 1, 'person for removed team removed!');
+  });
+
 });
