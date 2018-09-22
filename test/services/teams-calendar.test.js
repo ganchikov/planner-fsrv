b045dd9714aa740e62dc8e96a77675ddba896dd3
Mikhail Ganchikov <ganchikovm@gmail.com>
2018-09-22 12:26:28 +0300

    test fix for teams-calendar

--- test/services/teams-calendar.test.js
+++ test/services/teams-calendar.test.js
@@ -1,10 +1,22 @@
 const assert = require('assert');
 const app = require('../../src/app');
+const routeBuilder = require('@helpers/routebuilder');
+const {teams_calendar, authenticate} = require('@constants/services');
+const {authentication} = require('@constants/config');
+const jwtGen = require('@helpers/jwt-gen');
+
 
 describe('\'teams-calendar\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('teams-calendar');
 
-    assert.ok(service, 'Registered the service');
+  const authService = app.service(routeBuilder(app, authenticate));
+  const teamCalService = app.service(routeBuilder(app, teams_calendar));
+
+  let authConfig = app.get(authentication);
+  const jwt = new jwtGen(authConfig);
+  const headers = {authorization: 'BEARER ' + jwt.getAccessToken().compact()};
+
+  it('registered the service', async () => {
+    await authService.create({id_token: jwt.getIdToken().compact()}, {headers});
+    assert.ok(teamCalService, 'Registered the service');
   });
 });
